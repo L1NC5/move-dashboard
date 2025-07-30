ff2b5cbccf742a0ba913720e0497992eb3c53717
Federico Barbieri <150538449+L1NC5@users.noreply.github.com>
2025-07-30 17:58:25 +0200

    feat: table styling overhaul, current graph indicator, explicit function typing

--- src/components/MainDashboard.vue
+++ src/components/MainDashboard.vue
@@ -3,16 +3,17 @@ import { computed, onMounted, ref } from 'vue'
 import SensorTable from '@/components/SensorTable.vue'
 import { useSensorsStore } from '@/stores/SensorsStore.ts'
 import { useMeasurementsStore } from '@/stores/MeasurementsStore.ts'
+import type { Sensor } from '@/services/SensorService.ts'
 import SensorGraph from '@/components/SensorGraph.vue'
 
 const SensorsStore = useSensorsStore()
 const MeasurementsStore = useMeasurementsStore()
 const SelectedSensorId = ref<string>('')
-const SelectedSensor = computed(() =>
+const SelectedSensor = computed<Sensor | undefined>(() =>
   SensorsStore.sensorsData.find((s) => s.id === SelectedSensorId.value),
 )
 
-const setSelectedSensor = (sensorId: string) => {
+const setSelectedSensor = (sensorId: string): void => {
   SelectedSensorId.value = sensorId
 }
 
@@ -24,13 +25,16 @@ onMounted(async () => {
 </script>
 
 <template>
-  <SensorGraph
-    :measurements="MeasurementsStore.measurementsData[SelectedSensorId]"
-    :threshold="SelectedSensor?.threshold"
-  />
-  <SensorTable
-    :sensors="SensorsStore.sensorsData"
-    :latest-measurements="MeasurementsStore.latestMeasurementBySensor"
-    @change-graph-sensor="setSelectedSensor"
-  />
+  <section class="px-base flex flex-col gap-y-8">
+    <SensorGraph
+      :measurements="MeasurementsStore.measurementsData[SelectedSensorId]"
+      :threshold="SelectedSensor?.threshold"
+    />
+    <SensorTable
+      :sensors="SensorsStore.sensorsData"
+      :selected-sensor="SelectedSensor"
+      :latest-measurements="MeasurementsStore.latestMeasurementBySensor"
+      @change-graph-sensor="setSelectedSensor"
+    />
+  </section>
 </template>
--- src/components/SensorTable.vue
+++ src/components/SensorTable.vue
@@ -2,17 +2,27 @@
 import { computed, ref } from 'vue'
 import { type Sensor } from '@/services/SensorService.ts'
 import type { LatestMeasurementMap } from '@/stores/MeasurementsStore.ts'
+import type { Measurement } from '@/services/MeasurementsService.ts'
 import CaretDown from '@/components/icons/CaretDown.vue'
+import ChartIcon from '@/components/icons/ChartIcon.vue'
 
-type SensorTableColumnKey = keyof Sensor | 'lastValue' | 'status' | 'button'
+interface EnrichedSensor extends Sensor {
+  latest: Measurement | undefined
+  isAlert: boolean | undefined
+}
+
+type SensorTableColumnKey = keyof EnrichedSensor | 'button'
 
 interface SensorTableColumn {
   key: SensorTableColumnKey
   label: string
 }
 
+type SensorTableCellValue = string | number | boolean | undefined
+
 const props = defineProps<{
   sensors: Array<Sensor>
+  selectedSensor: Sensor | undefined
   latestMeasurements: LatestMeasurementMap
 }>()
 
@@ -23,15 +33,23 @@ const emit = defineEmits<{
 const sortKey = ref<SensorTableColumnKey>('id')
 const sortAsc = ref<boolean>(true)
 
-const sortedSensors = computed(() => {
-  const sorted = [...props.sensors]
+const enrichedSensors = computed<Array<EnrichedSensor>>(() => {
+  const sensorsWithLatest: Array<EnrichedSensor> = props.sensors.map((sensor) => {
+    const latest = props.latestMeasurements[sensor.id]
+    return {
+      ...sensor,
+      latest,
+      isAlert: latest ? latest.disp_mm > sensor.threshold : undefined,
+    }
+  })
 
-  sorted.sort((a, b) => {
+  sensorsWithLatest.sort((a, b) => {
     const aVal = getCellValue(a, sortKey.value)
     const bVal = getCellValue(b, sortKey.value)
 
-    if (aVal == null) return 1
-    if (bVal == null) return -1
+    if (aVal === 'N/A' && bVal !== 'N/A') return 1
+    if (bVal === 'N/A' && aVal !== 'N/A') return -1
+    if (aVal === 'N/A' && bVal === 'N/A') return 0
 
     if (typeof aVal === 'number' && typeof bVal === 'number') {
       return sortAsc.value ? aVal - bVal : bVal - aVal
@@ -42,10 +60,10 @@ const sortedSensors = computed(() => {
       : String(bVal).localeCompare(String(aVal))
   })
 
-  return sorted
+  return sensorsWithLatest
 })
 
-const toggleSort = (key: SensorTableColumnKey) => {
+const toggleSort = (key: SensorTableColumnKey): void => {
   if (sortKey.value == key) {
     sortAsc.value = !sortAsc.value
   } else {
@@ -58,23 +76,19 @@ const columns: Array<SensorTableColumn> = [
   { key: 'id', label: 'ID' },
   { key: 'name', label: 'Name' },
   { key: 'location', label: 'Location' },
-  { key: 'lastValue', label: 'Last value' },
-  { key: 'status', label: 'Status' },
+  { key: 'latest', label: 'Last value' },
+  { key: 'isAlert', label: 'Status' },
   { key: 'button', label: '' },
 ]
 
-const getCellValue = (sensor: Sensor, key: SensorTableColumnKey) => {
-  if (key in sensor) {
+const getCellValue = (sensor: EnrichedSensor, key: SensorTableColumnKey): SensorTableCellValue => {
+  if (key in sensor && key !== 'latest') {
     return sensor[key as keyof Sensor]
   }
 
-  const latest = props.latestMeasurements[sensor.id]
-
   switch (key) {
-    case 'lastValue':
-      return latest ? latest.disp_mm : 'N/A'
-    case 'status':
-      return latest ? latest.disp_mm > sensor.threshold : false
+    case 'latest':
+      return sensor.latest ? sensor.latest.disp_mm : 'N/A'
     default:
       return undefined
   }
@@ -82,41 +96,70 @@ const getCellValue = (sensor: Sensor, key: SensorTableColumnKey) => {
 </script>
 
 <template>
-  <table class="w-full overflow-x-auto">
-    <thead>
-      <tr class="bg-neutral-200">
-        <th v-for="column in columns" :key="column.key" class="border border-neutral-300">
-          <template v-if="column.key !== 'button'">
-            <button
-              class="size-full inline-flex justify-between items-center gap-x-4 px-4 py-2 bg-neutral-200 text-nowrap"
-              @click="toggleSort(column.key)"
+  <div class="min-w-0 overflow-x-auto overscroll-x-none rounded-lg shadow-sm">
+    <table class="w-full">
+      <thead>
+        <tr>
+          <th v-for="column in columns" :key="column.key" class="bg-blue-100">
+            <template v-if="column.key !== 'button'">
+              <button
+                class="size-full inline-flex justify-between items-center gap-x-4 p-4 text-nowrap"
+                @click="toggleSort(column.key)"
+              >
+                {{ column.label }}
+                <CaretDown
+                  class="transition-transform duration-100"
+                  :class="{
+                    'rotate-180': !sortAsc,
+                    'opacity-15': sortKey !== column.key,
+                  }"
+                />
+              </button>
+            </template>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="sensor in enrichedSensors"
+          :key="sensor.id"
+          class="group/row"
+          :class="sensor.id === selectedSensor?.id && 'bg-blue-50'"
+        >
+          <td v-for="column in columns" :key="column.key" class="p-4">
+            <template v-if="column.key === 'button'">
+              <button
+                class="w-full px-4 py-2 inline-flex justify-center items-center gap-x-2 whitespace-nowrap bg-blue-200 text-blue-950 rounded-sm hover:bg-blue-300 active:bg-blue-400 transition-colors"
+                @click="emit('changeGraphSensor', sensor.id)"
+              >
+                <ChartIcon />
+                Show data
+              </button>
+            </template>
+
+            <span
+              v-else-if="column.key === 'isAlert'"
+              class="inline-flex items-center gap-x-1.5 text-nowrap"
             >
-              {{ column.label }}
-              <CaretDown
-                class="transition-transform duration-100"
-                :class="{
-                  'rotate-180': !sortAsc,
-                  'opacity-0': sortKey !== column.key,
-                }"
+              <span
+                class="size-2 rounded-full"
+                :class="
+                  sensor.latest ? (sensor.isAlert ? 'bg-red-500' : 'bg-green-500') : 'bg-gray-400'
+                "
               />
-            </button>
-          </template>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="sensor in sortedSensors" :key="sensor.id" class="even:bg-white odd:bg-neutral-50">
-        <td v-for="column in columns" :key="column.key" class="px-4 py-2 border border-neutral-200">
-          <template v-if="column.key === 'button'">
-            <button @click="emit('changeGraphSensor', sensor.id)">Show data</button>
-          </template>
-          <template v-else>
-            {{ getCellValue(sensor, column.key) }}
-          </template>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+              <span>
+                {{ sensor.latest ? (sensor.isAlert ? 'Alert' : 'Ok') : 'N/A' }}
+              </span>
+            </span>
+
+            <template v-else>
+              {{ getCellValue(sensor, column.key) }}
+            </template>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped></style>
